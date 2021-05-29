f1fa54f2fa4b0325b0639007c1833ef26b4ca3fd
MarkBWilliams <mark.b.will1@gmail.com>
2021-05-29 01:39:33 -0700

    library app update, localstorage, firebase

---
+++ notes/local_storage.js
@@ -0,0 +1,22 @@
+/* 
+The two mechanisms within Web Storage are as follows:
+
+sessionStorage- maintains a separate storage area for each given origin that's available for the duration 
+of the page session (as long as the browser is open, including page reloads and restores).
+
+localStorage- does the same thing, but persists even when the browser is closed and reopened.
+
+
+The Storage.getItem() - used to get item from the storage
+
+Note: Storage.legnth- can test if object is empty or not
+
+Storage.setItem()- is used to create new data items, and if exist updates values
+
+Storage event- is fired when change is maed to the stroage object
+
+Storage.removeItem()- takes single argument- the key data item you want to remove, and 
+removes it from the storage object
+
+stroage.clear- takes two arguments, and empties the entire object
+*/
